b78633c5d35659873d7eb6afa3a328cc44d2934d
MaksZiem <mtziembicki@gmail.com>
2024-03-24 01:12:42 +0100

    setting game board

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,17 @@
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+export default function GameBoard() {
+    return (
+        <ol id="game-board">
+            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+                <ol>
+                        {row.map((playerSymol, colIndex) => <li key={colIndex}><button>{playerSymol}</button></li>)}
+                </ol>
+            </li>)}
+        </ol>
+    )
+}
